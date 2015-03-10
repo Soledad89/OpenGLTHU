970537d5018cd63454fb80cbe26c5991f136d347
soledad89 <wangboshine@126.com>
2015-03-10 21:01:10 +0800

    add new file week3.cpp

---
+++ Homework/week3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <OpenGl/glu.h>
+#include <OpenGl/gl.h>
+#include <GLUT/glut.h>
+
+using namespace std;
+
+int main()
+{
+
+	cout<<"I love OpenGL"<<endl;
+	return 0;
+}
+
