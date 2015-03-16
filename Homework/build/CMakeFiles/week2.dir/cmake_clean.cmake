0aafe202a0ce9cc92bf20123845964c3db0a4ec7
soledad89 <wangboshine@126.com>
2015-03-16 21:11:16 +0800

    Add CMakefiles

---
+++ Homework/build/CMakeFiles/week2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/week2.dir/week2.cpp.o"
+  "week2.pdb"
+  "week2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/week2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Homework/week3.cpp
+++ Homework/week3.cpp
@@ -1,11 +1,51 @@
-#include <OpenGl/glu.h>
-#include <OpenGl/gl.h>
-#include <GLUT/glut.h>
-
-using namespace std;
-
-int main()
-{
-
+//#include <windows.h> //0use proper includes for your system
+//#include <math.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/Glut.h>
+const int screenWidth = 640;	   // width of the screen window in pixels 
+const int screenHeight = 480;	   // height of the screen window in pixels
+//<<<<<<<<<<<<<<<<<<<<<<< myInit >>>>>>>>>>>>>>>>>>>>
+ void myInit(void)
+ {
+    glClearColor(0.0,0.0,0.0,0.0);       // the background color is white
+	glPointSize(4.0);		       
+	glMatrixMode(GL_PROJECTION); 	   
+	glLoadIdentity();
+	gluOrtho2D(0.0f, (GLdouble)screenWidth,0.0f,(GLdouble)screenHeight);
 }
-
+//<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);     // clear the screen 
+	glTranslatef(350,100,0);
+	glBegin(GL_TRIANGLES);								// Drawing Using Triangles
+		glColor3f(1.0f, 1.0f, 0.0f);         // the drawing color
+		glVertex3i(0,200,0 );					// Top
+		glVertex3i(-100,0,0);					// Bottom Left
+		glVertex3i(100,0,0);					// Bottom Right
+	glEnd();											// Finished Drawing The Triangle
+	glTranslatef(300,0,0);					
+	
+	glBegin(GL_QUADS);									// Draw A Quad
+		glColor3f(0.0f, 1.0f, 0.0f);         // the drawing color
+		glVertex3f(-100,0,0);					// Top Left
+		glVertex3f(100,0,0);					// Top Right
+		glVertex3f(100,200,0);					// Bottom Right
+		glVertex3f(-100,200,0);					// Bottom Left
+	glEnd();	
+	glFlush();		   // send all output to display 
+}
+//<<<<<<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>>>>>
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);          // initialize the toolkit
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // set the display mode
+	glutInitWindowSize(screenWidth, screenHeight); // set the window size
+	glutInitWindowPosition(200, 150); // set the window position on screen
+	glutCreateWindow("Plot the Traianle and the roct"); // open the screen window
+	glutDisplayFunc(myDisplay);     // register the redraw function
+	myInit();                   
+	glutMainLoop(); 		     // go into a perpetual loop
+	return 0;
+}
