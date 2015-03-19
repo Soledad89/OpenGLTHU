27ef44fd91f18ab701549f6923ad4f75bed26368
soledad89 <wangboshine@126.com>
2015-03-19 08:38:04 +0800

    change the week3.cpp file

--- Homework/HomeworkXcodeProject/week3.cpp
+++ Homework/HomeworkXcodeProject/week3.cpp
@@ -9,54 +9,53 @@
 // to see this can be immediatelly shown in xcode .
 // to test this file can be commited by xcode.
 
+// 全局变量
 const int screenWidth = 640;	   // width of the screen window in pixels 
 const int screenHeight = 480;	   // height of the screen window in pixels
 GLfloat tri_angle = 0.0;
-GLfloat squ_angle = 0.0;
+GLfloat quads_angle = 0.0;
+
+struct GLintPoint {
+    GLint x, y;
+};
+
+GLintPoint corner[2];
+bool selected = false;
 
 
-//<<<<<<<<<<<<<<<<<<<<<<< myInit >>>>>>>>>>>>>>>>>>>>
- void myInit(void)
- {
-    glClearColor(0.0,0.0,0.0,0.0);       // the background color is white
-	glPointSize(4.0);		       
-	glMatrixMode(GL_PROJECTION); 	   
-	glLoadIdentity();
-	gluOrtho2D(0.0f, (GLdouble)screenWidth,0.0f,(GLdouble)screenHeight);
-}
-
-
+//回调函数
 //<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
 void myDisplay(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);     // clear the screen 
-
-	glBegin(GL_TRIANGLES);								// Drawing Using Triangles
-		glColor3f(1.0f, 1.0f, 0.0f);         // the drawing color
-		glVertex3f(100,100,0);					// Top
-		glVertex3f(300,100,0);					// Bottom Left
-		glVertex3f(200,300,0);					// Bottom Right
-	glEnd();											// Finished Drawing The Triangle
-	
-	glBegin(GL_QUADS);									// Draw A Quad
-		glColor3f(0.0f, 1.0f, 0.0f);         // the drawing color
-		glVertex3f(400,100,0);					// Top Left
-		glVertex3f(600,100,0);					// Top Right
-		glVertex3f(600,300,0);					// Bottom Right
-		glVertex3f(400,300,0);					// Bottom Left
-	glEnd();	
-
-	glBegin(GL_QUADS);									// Draw A Quad
-		glColor3f(0.0f, 1.0f, 0.0f);         // the drawing color
-		glVertex3f(-0.5,-0.5,0);					// Top Leftgit
-		glVertex3f(0.5,-0.5,0);					// Top Right
-		glVertex3f(0.5,0.5,0);					// Bottom Right
-		glVertex3f(-0.5,0.5,0);					// Bottom Left
-	glEnd();	
-	
-	glFlush();		   // send all output to display
-
-	glutSwapBuffers();
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glLoadIdentity();
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glTranslatef(-0.5, 0.5, 0.0);
+    glRotatef(tri_angle,0.0,0.0,1.0);
+    glBegin(GL_TRIANGLES);
+        glVertex3f(-0.4, -0.4, 0);
+        glVertex3f(0.4, -0.4, 0);
+        glVertex3f(0, 0.4, 0);
+    glEnd();
+    tri_angle ++;
+    
+    
+    glLoadIdentity();
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glTranslatef(0.5, 0.5, 0.0);
+    glRotatef(quads_angle,0.0,0.0,1.0);
+    glBegin(GL_QUADS);
+        glVertex3f(-0.4,-0.4,0.0);
+        glVertex3f(0.4,-0.4,0.0);
+        glVertex3f(0.4,0.4,0.0);
+        glVertex3f(-0.4, 0.4, 0);
+    glEnd();
+    quads_angle += 2;
+    
+    glFlush();
+    glutSwapBuffers();
 
 }
 
@@ -70,36 +69,54 @@ void processMenuEvents(int option) {
 		case WHITE : red = 1.0; green = 1.0; blue = 1.0; break;
 	}
 */
+/*
+void myMouse(int button, int state, GLint x, GLint y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        corner[0].x = x;
+        corner[0].y = screenHeight - y;
+        selected = true;
+        
+    }
+    
+    glutPostRedisplay();
+}
+ 
+void myMovedMouse(GLint x, GLint y)
+{
+    corner[1].x = x;
+    corner[1].y = screenHeight - y;
+    
+    glutPostRedisplay();
+}
+*/
 
-
-
+//主函数
 //<<<<<<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>>>>>
 int main(int argc, char** argv)
 
 {
-	glutInit(&argc, argv);          // initialize the toolkit
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB); // set the display mode
-	glutInitWindowSize(screenWidth, screenHeight); // set the window size
-	glutInitWindowPosition(200, 150); // set the window position on screen
-	glutCreateWindow("Plot the Traianle and the roct"); // open the screen window
-	glutDisplayFunc(myDisplay);     // register the redraw function
-	glutIdleFunc(myDisplay);
-	
-	myInit();                   
-	
-	// 
-	// add menu and other components here
-	//call our function to create the menu
-	/*	
-	glutCreateMenu(processMenuEvents);
-	glutAddMenuEntry("Red",RED);
-	glutAddMenuEntry("Blue",BLUE);
-	glutAddMenuEntry("Green",GREEN);
-	glutAddMenuEntry("White",WHITE);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
-	*/
-	//
+    //设置窗口
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
+    glutInitWindowSize(screenWidth, screenHeight); // set the window size
+    glutInitWindowPosition(200, 150); // set the window position on screen
+    glutCreateWindow("Plot the Traianle and the roct"); // open the screen window
+    glutDisplayFunc(myDisplay);
+    glutIdleFunc(myDisplay);
 
-	glutMainLoop(); 		     // go into a perpetual loop
-	return 0;
+	
+    //设置渲染
+    glClearColor(0.0,0.0,0.0,0.0);       // the background color is white
+    glPointSize(4.0);
+    //glViewport(0, 0, (GLdouble)screenWidth, (GLdouble)screenHeight);
+
+    //glutMouseFunc(myMouse);
+    //glutPassiveMotionFunc(myMovedMouse);
+    //glutMotionFunc(myMovedMouse);
+    
+    
+    glutMainLoop(); 		     // go into a perpetual loop
+    return 0;
 }
