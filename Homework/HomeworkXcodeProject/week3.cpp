bd7226f81e3d98f3b41aef3e9b449c3ec102f937
soledad89 <wangboshine@126.com>
2015-03-20 14:54:24 +0800

    update the week3.cpp

--- Homework/HomeworkXcodeProject/week3.cpp
+++ Homework/HomeworkXcodeProject/week3.cpp
@@ -39,9 +39,10 @@ int pa = 0;
 int px = 0;
 int py = 0;
 
+float a, b;
 float d = 0.0;
 float len = 0.0;
-float angle = 0.0;
+//float angle = 0.0;
 float red = 0.0, blue = 1.0, green = 1.0;
 
 //<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
@@ -66,63 +67,81 @@ void myDisplay(void)
 {
     
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
+    float angle;
     glLoadIdentity();
     if (mouseleftdown)
     {
+        len = pow((corner[1].y - corner[0].y),2) + pow((corner[1].x - corner[0].x),2);
+        angle = sqrt(len)/winw * 360;
         
         if ( corner[0].x >= winw/2 && corner[0].y >= winh/2)
-            pa = 1, px = 1, py = 1;
+        {
+            pa = 1; px = 1; py = 1;
+        }
         else if (corner[0].x <= winw/2 && corner[0].y >= winh/2)
-            pa = 2, px = -1, py = 1;
+        {
+            pa = 2;px = -1;py = 1;
+        }
         else if (corner[0].x <= winw/2 && corner[0].y <= winh/2)
-            pa = 3, px = -1, py = -1;
+        {
+            pa = 3;px = -1;py = 1;
+        }
         else
-            pa = 4, px = 1, py = -1;
+        {
+            pa = 4;px = 1;py = -1;
+        }
+        
         
         if (corner[1].x == corner[0].x)
         {
             if (corner[1].y >= corner[0].y)
-                d = PI/2;
+            {
+                d = PI/2; a = 1; b = 0;
+            }
             else
-                d = -PI/2;
+            {
+                d = -PI/2; a = -1; b = 0;
+            }
         }
+        
+        
         if (corner[1].y == corner[0].y)
         {
-            if (corner[1].x >= corner[1].x)
-                d = 0;
+            if (corner[1].x >= corner[0].x)
+            {
+                d = 0; a = 0; b = 1;
+            }
             else
-                d = PI;
+            {
+                d = 0; a = 0; b = -1;
+            }
         }
+        
         if (corner[1].y != corner[0].y && corner[1].x != corner[0].x)
-            d = atan((double)(corner[1].y - corner[0].y)/(double)(corner[1].x - corner[0].x));
-        
-        len = pow((corner[1].y - corner[0].y),2) + pow((corner[1].x - corner[0].x),2);
-        angle = sqrt(len)/winw * 360;
-        
+        {
+            d = ((double)(corner[1].y - corner[0].y)/(double)(corner[1].x - corner[0].x));
+            a = px; b = - px / (d * py);
+        }
     }
     
-	glLoadIdentity();
+    glLoadIdentity();
     glTranslatef(-0.5, 0.5, 0.0);
-    if ( pa == 2)
-		glRotatef(angle,px,d,0.0);
-
-	glBegin(GL_TRIANGLES);
+    if (pa == 2)
+        glRotatef(angle,a,b,0.0);
+    glBegin(GL_TRIANGLES);
         glColor3f(0.0f, 1.0f, 0.0f);
         glVertex3f(-0.4, -0.4, 0);
         glColor3f(1.0f, 0.0f, 0.0f);
         glVertex3f(0.4, -0.4, 0);
         glColor3f(0.0f, 0.0f, 1.0f);
         glVertex3f(0, 0.4, 0);
-	glEnd();
-		
-    
+    glEnd();
     
     glLoadIdentity();
     glColor3f(red, green, blue);
     glTranslatef(0.5, 0.5, 0.0);
     if ( pa == 1)
-        glRotatef(angle,d,px,0.0);
+        glRotatef(angle,a,b,0.0);
     
     glBegin(GL_QUADS);
         glColor3f(0.5, 0.5, 1);
